c1e4b544084d4ed292dd92113f8a929c2acb093d
Anshu <anshukumarsingh448@gmail.com>
2022-03-22 13:19:13 +0530

    Hello World

--- app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,16 +1,49 @@
 package com.example.kotlinbasics
+fun main() {
+    /* val age =19
 
-fun main(){
-    val age =25
     if (age >= 21){
         println("Now you may drink in the INDIA")
     }else if (age >=18){
         println("You can Vote")
 
     }else if (age<=16){
-        println("you are too young")
+        println("you can Drive")
     }
     else{
-        println("I Love You")
+        println("You're too young")
+    }
+
+var Season = 4
+    when(Season){
+        1-> println("Summer")
+        2-> println("Winter")
+        3-> println("Spring")
+        4-> {
+            println("Fall")
+            println("Rainy")
+        }
+    }
+
+var month = 3
+    when(month){
+      in 3..5 -> println("Summer")
+      in 6..8 -> println("Rainy")
+      in 9..11 -> println("Winter")
+        in 12 downTo 2 -> println("Spring")
+
+    }
+}*/
+
+    var x: Any = 13.25.toInt()
+    when (x) {
+       is Double -> println("$x is double")
+        is String -> println("$x is String")
+        is Int -> println("$x is int")
     }
 }
+
+
+
+
+
