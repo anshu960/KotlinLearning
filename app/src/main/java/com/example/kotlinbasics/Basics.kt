317fcc8ba37d0cc896f8a7e28e71a60ff36dc6b9
Anshu <anshukumarsingh448@gmail.com>
2022-03-22 11:28:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+
+fun main(){
+    val age =25
+    if (age >= 21){
+        println("Now you may drink in the INDIA")
+    }else if (age >=18){
+        println("You can Vote")
+
+    }else if (age<=16){
+        println("you are too young")
+    }
+    else{
+        println("I Love You")
+    }
+}
